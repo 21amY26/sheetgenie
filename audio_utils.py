a2af11d6cf1eda1e47bc75259329bc73be620e1e
AMISHA SINGH <amishasingh2105@gmail.com>
2026-04-27 21:13:47 +0530

    stable pitch segmentation

--- audio_utils.py
+++ audio_utils.py
@@ -43,3 +43,56 @@ def create_seq_sheet_mono(predicted_notes,path):
 
     s.write("musicxml", path)
     print("Sheet music created at",path)
+
+
+def merge_consecutive(notes, durations):
+
+    merged_notes = []
+    merged_durations = []
+
+    for n, d in zip(notes, durations):
+
+        if merged_notes:
+            prev_n = merged_notes[-1]
+            prev_d = merged_durations[-1]
+
+            # merge ONLY if:
+            # same note AND clearly smaller fragment
+            if n == prev_n and d < 0.6 * prev_d:
+                merged_durations[-1] += d
+                continue
+
+        merged_notes.append(n)
+        merged_durations.append(d)
+
+    return merged_notes, merged_durations
+
+
+
+def seconds_to_beats(d, bpm=120):
+    return d / (60 / bpm)
+
+
+
+def quantize_duration(d):
+    vals=[0.25,0.5,1,2,4,8]
+    return min(vals, key=lambda x: abs(x-d))
+
+
+
+def create_seq_sheet(notes, durations,file_path):
+
+    s = stream.Stream()
+
+    for p, d in zip(notes, durations):
+
+        n = note.Note()
+        n.pitch.midi = int(p)
+        beats=seconds_to_beats(d,bpm=120)
+        n.quarterLength=quantize_duration(beats)
+
+        s.append(n)
+
+    s.write("musicxml", file_path+'.xml')
+
+    print("Sheet music created at", file_path+'.xml')
